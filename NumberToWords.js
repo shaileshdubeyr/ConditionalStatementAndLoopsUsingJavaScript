4d39eafcb42b09ebe7fdccda2e55adc7523468e6
SHAILESH <shaileshdubeyr@gmail.com>
2021-11-25 07:30:28 +0530

    Added : code for number to words

---
+++ NumberToWords.js
@@ -0,0 +1,26 @@
+let numner = Math.floor( (Math.random() * 10 ) % 10 );
+
+if(numner == 0){
+    console.log("Zero");
+}else if(numner == 1){
+    console.log("one");
+}else if(numner == 2){
+    console.log("two");
+}else if(numner == 3){
+    console.log("three");
+}else if(numner == 4){
+    console.log("four");
+}else if(numner == 5){
+    console.log("five");
+}else if(numner == 6){
+    console.log("six");
+}else if(numner == 7){
+    console.log("seven");
+}else if(numner == 8){
+    console.log("eight");
+}else if(numner == 9){
+    console.log("nine");
+}
+else{
+    console.log("invalid number");
+}
