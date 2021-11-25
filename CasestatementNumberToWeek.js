b8fb5839dd5658e5046cb6431c78cd9ce6e9ae19
SHAILESH <shaileshdubeyr@gmail.com>
2021-11-25 11:19:57 +0530

    Added : code for number to week using case statement

---
+++ CasestatementNumberToWeek.js
@@ -0,0 +1,28 @@
+let number = Math.floor( (Math.random() * 10 ) % 7 ) + 1;
+
+switch(number){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday")
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("invalid input");
+        break;
+}
