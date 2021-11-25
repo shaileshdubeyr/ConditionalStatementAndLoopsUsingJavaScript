c47dd4a49c8143f2b44a203c9e2834ddb23260fe
SHAILESH <shaileshdubeyr@gmail.com>
2021-11-25 07:08:00 +0530

    Added code to check leap year

---
+++ LeapYear.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("enter the year ", function(day) {
+        if(`${day}` % 4 == 0) {
+            console.log("it is leap year");
+        }
+        else{
+            console.log("it is not a leap year");
+        }
+        console.log(`the year is " ${day}`)
+        rl.close();
+});
